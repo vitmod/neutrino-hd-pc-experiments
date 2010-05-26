cb99d1421015c9120319200f66241642ee4eb559
Carjay <carjay@gmx.net>
2010-05-27 00:24:54 +0200

    - add lots of debug info and some stubs

--- lib/libdvbapi/audio_dvbapi.cpp
+++ lib/libdvbapi/audio_dvbapi.cpp
@@ -23,6 +23,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "audio_dvbapi.h"
+#include <iostream>
+#include <boost/format.hpp>
 
 cAudio *audioDecoder = 0;
 
@@ -32,44 +34,52 @@ cAudio::cAudio(void * hBuffer, void * encHD, void * encSD)
 
 cAudio::~cAudio(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 void * cAudio::GetHandle()
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 void * cAudio::GetDSP()
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 void cAudio::HandleAudioMessage(int Event, void *pData)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 void cAudio::HandlePcmMessage(int Event, void *pData)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 /* shut up */
 int cAudio::mute(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::unmute(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::SetMute(int enable)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
@@ -77,12 +87,14 @@ int cAudio::SetMute(int enable)
 /* bypass audio to external decoder */
 int cAudio::enableBypass(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::disableBypass(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
@@ -90,12 +102,14 @@ int cAudio::disableBypass(void)
 /* volume, min = 0, max = 255 */
 int cAudio::setVolume(unsigned int left, unsigned int right)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::getVolume(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
@@ -103,76 +117,89 @@ int cAudio::getVolume(void)
 /* start and stop audio */
 int cAudio::Start(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::Stop(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 bool cAudio::Pause(bool Pcm)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return true;
 }
 
 
 bool cAudio::Resume(bool Pcm)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return true;
 }
 
 
 void cAudio::SetStreamType(AUDIO_FORMAT type)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 AUDIO_FORMAT cAudio::GetStreamType(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return AUDIO_FMT_AUTO;
 }
 
 
 bool cAudio::ReceivedAudioDelay(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return true;
 }
 
 
 void cAudio::SetReceivedAudioDelay(bool set)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 unsigned int cAudio::GetAudioDelay(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 void cAudio::SetSyncMode(AVSYNC_TYPE Mode)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 /* stream source */
 int cAudio::getSource(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::setSource(int source)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::Flush(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
@@ -180,66 +207,83 @@ int cAudio::Flush(void)
 /* select channels */
 int cAudio::setChannel(int channel)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::getChannel(void)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::PrepareClipPlay(int uNoOfChannels, int uSampleRate, int uBitsPerSample, int bLittleEndian)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::WriteClip(unsigned char * buffer, int size)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cAudio::StopClip()
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 void cAudio::getAudioInfo(int &type, int &layer, int& freq, int &bitrate, int &mode)
 {
+	type = 0;
+	layer = 0;
+	freq = 0;
+	bitrate = 0;
+	mode = 0;
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 void cAudio::SetSRS(int iq_enable, int nmgr_enable, int iq_mode, int iq_level)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 bool cAudio::IsHdmiDDSupported()
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 	return true;
 }
 
 
 void cAudio::SetHdmiDD(bool enable)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 void cAudio::SetSpdifDD(bool enable)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 void cAudio::ScheduleMute(bool On)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
 
 void cAudio::EnableAnalogOut(bool enable)
 {
+	std::cout << "cAudio::" << __FUNCTION__ << std::endl;
 }
 
--- lib/libdvbapi/video_dvbapi.cpp
+++ lib/libdvbapi/video_dvbapi.cpp
@@ -23,6 +23,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "video_dvbapi.h"
+#include <iostream>
+#include <boost/format.hpp>
 
 unsigned int system_rev = 0x00;
 cVideo *videoDecoder = 0;
@@ -30,54 +32,67 @@ cVideo *videoDecoder = 0;
 /* constructor & destructor */
 cVideo::cVideo(int mode, void * hChannel, void * hBuffer)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 cVideo::~cVideo(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 void * cVideo::GetDRM(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 void * cVideo::GetTVEnc()
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 void * cVideo::GetTVEncSD()
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 void * cVideo::GetHandle()
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 void cVideo::SetAudioHandle(void * handle)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 /* aspect ratio */
 int cVideo::getAspectRatio(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 void cVideo::getPictureInfo(int &width, int &height, int &rate)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
+	width = 4096;
+	height = 2160;
+	rate = 24;
 }
 
 
 int cVideo::setAspectRatio(int aspect, int mode)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
@@ -85,30 +100,35 @@ int cVideo::setAspectRatio(int aspect, int mode)
 /* cropping mode */
 int cVideo::getCroppingMode(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cVideo::setCroppingMode(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 /* stream source */
 int cVideo::getSource(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cVideo::setSource(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cVideo::GetStreamType(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0; 
 }
 
@@ -116,12 +136,14 @@ int cVideo::GetStreamType(void)
 /* blank on freeze */
 int cVideo::getBlank(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cVideo::setBlank(int enable)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
@@ -129,32 +151,38 @@ int cVideo::setBlank(int enable)
 /* get play state */
 int cVideo::getPlayState(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 void cVideo::SetDRMDelay(unsigned int delay)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 	
 
 void cVideo::SetVideoDelay(unsigned int delay)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::HandleDRMMessage(int Event, void *pData)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::HandleVideoMessage(void * hHandle, int Event, void *pData)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 VIDEO_DEFINITION cVideo::GetVideoDef(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return VIDEO_SD; 
 }
 
@@ -162,42 +190,49 @@ VIDEO_DEFINITION cVideo::GetVideoDef(void)
 /* change video play state */
 int cVideo::Start(void * PcrChannel, unsigned short PcrPid, unsigned short VideoPid, void * hChannel)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cVideo::Stop(bool blank)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 bool cVideo::Pause(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return true;
 }
 
 
 bool cVideo::Resume(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return true;
 }
 
 
 int cVideo::LipsyncAdjust()
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int64_t cVideo::GetPTS(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cVideo::Flush(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
@@ -205,89 +240,106 @@ int cVideo::Flush(void)
 /* set video_system */
 int cVideo::SetVideoSystem(int video_system, bool remember)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 int cVideo::SetStreamType(VIDEO_FORMAT type)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return 0;
 }
 
 
 void cVideo::SetSyncMode(AVSYNC_TYPE mode)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::ShowPicture(const char * fname)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::StopPicture()
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::Standby(unsigned int bOn)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::Pig(int x, int y, int w, int h, int osd_w, int osd_h)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::setContrast(int val)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 bool cVideo::ReceivedDRMDelay(void)
 { 
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return true; 
 }
 
 
 bool cVideo::ReceivedVideoDelay(void)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 	return true; 
 }
 
 
 void cVideo::SetReceivedDRMDelay(bool Received)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 	
 	
 void cVideo::SetReceivedVideoDelay(bool Received)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::SetFastBlank(bool onoff)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::SetTVAV(bool onoff)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::SetWideScreen(bool onoff)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::SetVideoMode(analog_mode_t mode)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
 
 void cVideo::SetDBDR(int dbdr)
 {
+	std::cout << "cVideo::" << __FUNCTION__ << std::endl;
 }
 
